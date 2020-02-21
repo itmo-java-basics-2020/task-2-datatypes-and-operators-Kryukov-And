4dabf040db343f652e1236993da809c927193407
acer <Kryukov-And>
2020-02-21 23:19:23 +0300

    Check

--- src/main/java/ru/itmo/java/Task2.java
+++ src/main/java/ru/itmo/java/Task2.java
@@ -3,87 +3,49 @@ package ru.itmo.java;
 @SuppressWarnings("all")
 public class Task2 {
 
-    /**
-     * Дано произвольное вещественное число. Выведите его дробную часть
-     * <p>
-     * Например
-     * - дано 17.9
-     * - вывод 0.9
-     */
     double getFractionalPart(double realNumber) {
-        return 0.0; //your code here
+        return (realNumber - Math.floor(realNumber));
     }
 
-    /**
-     * Дан символ, верните его код. Не используйте явное приведение типов и любые библиотечные функции
-     */
     int charCode(char c) {
-        return 0; //your code here
+        return c;
     }
 
-    /**
-     * Даны 2 целых числа. Проверьте равны ли они
-     */
     boolean testIfIntsEqual(Integer a, Integer b) {
-        return false; //your code here
+        return a.equals(b);
     }
 
-    /**
-     * Дано целое число. Выведите {@code true}, если оно входит в заданный диапазон или {@code false}
-     * в иных случаях
-     *
-     * @param number      заданное число
-     * @param leftBound   правая граница диапазона
-     * @param rightBound  верхняя граница диапазона
-     * @param inclusively входят ли границы в заданный даипазон
-     */
     boolean numberInRange(Integer number, Integer leftBound, Integer rightBound, Boolean inclusively) {
-        return false; //your code here
+        if ((number == null) || (leftBound == null) || (rightBound == null) || (inclusively == null))
+            return false;
+        if (inclusively)
+            return (number >= leftBound) && (number <= rightBound);
+        else
+            return (number > leftBound) && (number < rightBound);
     }
 
-    /**
-     * Даны 3 символа. Определите является ли хотя бы один из них цифрой 1..9
-     */
     boolean atLeastOneIsDigit(char c1, char c2, char c3) {
-        return false; //your code here
+        return Character.isDigit(c1) || Character.isDigit(c2) || Character.isDigit(c3);
     }
 
-    /**
-     * Даны два вещественных числа. Определите равны ли они
-     */
     boolean areRealNumbersEqual(double a, double b) {
-        return false; //your code here
+        return Math.abs(a - b) < 1e-6;
     }
 
-    /**
-     * Дано трехзначное число 100 <= N <= 999. Найдите сумму его чисел.
-     */
     int sumOfDigits(int n) {
-        return 0; //your code here
+        return n % 10 + n / 10 % 10 + n / 100;
     }
 
-    /**
-     * Дано натуральное число N <= 10000. Выведите следующее после N четное число. Например: N = 8, Result = 10
-     */
     int nextEvenNumber(int n) {
-        return 0; //your code here
+        return n / 2 * 2 + 2;
     }
 
-    /**
-     * Школа №32 решила создать 3 дополнительных физмат класса и укомплектовать кабинеты новыми партами.
-     * За одной партой может сидеть 2 ученика. Кол-во учеников в каждом из классов известно (num1, num2, num3).
-     * Выведите минимальное кол-во парт, которые нужно приобрести школе, при условии что каждый из классов сидит в
-     * своем кабинете
-     */
     int schoolDesks(int num1, int num2, int num3) {
-        return 0; //your code here
+        return (num1 % 2 + num1 / 2) + (num2 % 2 + num2 / 2) + (num3 % 2 + num3 / 2);
     }
 
-    /**
-     * Дано натуральное число N >= 10. Выведите результат применения XOR к последним 2-м цифрам числа N
-     */
     int xorDigits(int N) {
-        return 0; //your code here
+        return (N % 10) ^ (N / 10 % 10);
     }
 
 }
